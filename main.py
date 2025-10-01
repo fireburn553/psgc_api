a1cba2384ae7f7572445b68038403dda9ce8f3dd
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2025-10-01 18:20:15 +0800

    added link for the development

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
-                   "https://fireburn553.github.io"],  # Or specify your React app URL: ["http://localhost:3000"]
+                   "https://fireburn553.github.io", "http://localhost:5173"],  # Or specify your React app URL: ["http://localhost:3000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
